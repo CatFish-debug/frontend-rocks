e6c95c5418c0d70cfbbfc69f44b2d8b2340a683f
CatFish-debug <dwaynejerrellrivera@gmail.com>
2026-02-26 16:26:35 +0000

    pokedeck!

--- src/Root.tsx
+++ src/Root.tsx
@@ -1,6 +1,6 @@
 /*import { useState, useEffect } from "react";
 import { Link } from "react-router";
-import { PokeAPI } from "./api";
+importfah { PokeAPI } from "./api";
 */
 
 import { useState, useEffect } from "react";
